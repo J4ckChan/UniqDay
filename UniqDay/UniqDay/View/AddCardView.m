93bc3ec5194f02c629df1d7fac24d600724e7cb5
JackChan829 <liang.chen829@gmail.com>
2016-12-08 02:22:43 +0800

    classify files

---
+++ UniqDay/UniqDay/View/AddCardView.m
@@ -0,0 +1,21 @@
+//
+//  AddCardView.m
+//  UniqDay
+//
+//  Created by ChanLiang on 08/12/2016.
+//  Copyright © 2016 ChanLiang. All rights reserved.
+//
+
+#import "AddCardView.h"
+
+@implementation AddCardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
